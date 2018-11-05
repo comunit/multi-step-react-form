976ab514ee3ac4095e9f0b1cfc21a8ae8e471c64
Imran Rafique <comunit76@gmail.com>
2018-11-05 19:45:12 +0000

    initial commit

---
+++ precache-manifest.edcca9e0b743fbf9c0a4e253e31a3de0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8fbc80b79cced6994c6c",
+    "url": "/static/js/main.8fbc80b7.chunk.js"
+  },
+  {
+    "revision": "bcefb647ab5acc322d1b",
+    "url": "/static/js/1.bcefb647.chunk.js"
+  },
+  {
+    "revision": "8fbc80b79cced6994c6c",
+    "url": "/static/css/main.42f0b2f9.chunk.css"
+  },
+  {
+    "revision": "2824bf19d22fb98066cc1e42a36787f1",
+    "url": "/index.html"
+  }
+];
